fd2c5683b1fd8c8dc159bed73d6c6560a0e90d2f
Florin C <corciova.f@gmail.com>
2024-08-05 09:20:35 +0300

    Created views to display list of saloons and saloon details

--- Wedding/Saloane/views.py
+++ Wedding/Saloane/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views import View
 from .models import BallRoom
 
@@ -11,6 +11,15 @@ class AllSalons(View):
         context = {'ballrom_list':ballrooms}
         return render(request,self.template_name,context=context)
 
+class SaloonDetails(View):
+    '''Class to construct a view to display a saloon details'''
+
+    model_name = BallRoom
+    template_name = 'Saloane/saloon_details.html'
+    def get(self,request,pk):
+        saloon = get_object_or_404(self.model_name,pk=pk)
+        context = {'saloon':saloon}
+        return render(request,self.template_name,context=context)
 def index(request):
     return render(request, 'index.html')
 
