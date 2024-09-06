122cb85f6b8f71eb9b337a8ddd1abd9a27360fc0
Florin C <corciova.f@gmail.com>
2024-09-06 06:23:14 +0300

    Created a model for catering services

--- Wedding/Catering/models.py
+++ Wedding/Catering/models.py
@@ -12,9 +12,9 @@ class Cofetarie(models.Model):
     phone = models.CharField(max_length=10)
     meniu = models.FileField(upload_to='pdf-menus')
     servicii = (
-        'Candy Bar','Candy Bar',
-        'Torturi personalizate','Torturi personalizate',
-        'Livrare','Livrare',
+        ('Candy Bar','Candy Bar'),
+        ('Torturi personalizate','Torturi personalizate'),
+        ('Livrare','Livrare'),
 
     )
     servicii_cofetarie = MultiSelectField(choices=servicii,default='')
@@ -31,6 +31,36 @@ class Cofetarie(models.Model):
 
 
 
-    
+class Catering(models.Model):
+    '''Class to define a model for food catering clients'''
+
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    adress = models.CharField(max_length=200)
+    phone = models.CharField(max_length=10)
+    meniu = models.FileField(upload_to='pdf-menus')
+    instagram_page = models.URLField()
+    tik_tok_page = models.URLField()
+    facebook_page = models.URLField()
+    website_page = models.URLField()
+
+    servicii = (
+        ('Bar si bauturi', 'Bar si bauturi'),
+        ('Bufet', 'Bufet'),
+        ('CandyBar','CandyBar'),
+        ('Chef','Chef'),
+        ('Degustare','Degustare'),
+        ('Livrare','Livrare'),
+        ('Aranjare','Aranjare'),
+        ('Personal Servire','Personal Servire'),
+    )
+
+    servicii_catering = MultiSelectField(choices=servicii,default='')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('catering_details',kwargs={'pk':self.pk})
 
 
